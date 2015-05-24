a9a05cb8a822837f108e9f9e5c821567dbb99cf0
yorickdewid <yorick17@outlook.com>
2015-05-24 11:46:09 +0200

    Read items from file

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	)
 
 func main() {
-    fmt.Println("hello world")
+	file, err := os.Open("targetlist.txt")
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
 }
